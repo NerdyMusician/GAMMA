3991316fb18611ac743e4f957cfee6028a55faa9
Jorden Finne <jorden.finne@gmail.com>
2022-03-22 17:36:21 -0500

    release cleanup

--- GAMMA/ViewModels/MainViewModel.cs
+++ GAMMA/ViewModels/MainViewModel.cs
@@ -18,7 +18,7 @@ namespace GAMMA.ViewModels
         // Constructors
         public MainViewModel()
         {
-            ApplicationVersion = "GAMMA 1.29.03 beta";
+            ApplicationVersion = "GAMMA 1.29.03";
             Configuration.MainModelRef = this;
             PlayerClasses = new();
             SpellcastingClasses = new();
